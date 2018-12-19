e20562169687eda213d1561c714ad4b16ee44bae
陈 晓波 <chenxiaobo@hyronsh.com>
2018-12-19 16:28:59 +0900

    before mybatis

--- spring4demo/src/main/java/listener/TimeHandler.java
+++ spring4demo/src/main/java/listener/TimeHandler.java
@@ -1,7 +1,14 @@
 package listener;
 
+import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.JoinPoint;
+
 public class TimeHandler {
-	public void printTime() {
-		System.out.println("CurrentTime = " + System.currentTimeMillis());
+	public void printTime(JoinPoint jp) {
+		System.out.println("Log before method : CurrentTime = " + System.currentTimeMillis());
+	}
+	
+	public void printTimeAfter() {
+		System.out.println("Log after method : CurrentTime = " + System.currentTimeMillis());
 	}
 }
