8c3fea6a73b33059a4bd9d8cf7ed9fffc4cbbb16
deletefromuser <abc@example.com>
2018-12-20 20:01:54 +0900

    springmvc @PathVariable

---
+++ spring4demo/src/main/java/controller/HelloController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printHello(ModelMap model) {
+		model.addAttribute("message", "Hello Spring MVC Framework!");
+		return "hello";
+	}
+
+	@RequestMapping(value = "/calc/{a}/{b}", method = { RequestMethod.GET })
+	public String calc(@PathVariable int a, @PathVariable int b, ModelMap model) {
+		model.addAttribute("message", "sum is " + (a + b));
+		return "hello";
+	}
+}
