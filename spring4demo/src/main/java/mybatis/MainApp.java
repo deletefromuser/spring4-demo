652bc36d685972ca6839de83d1e3a4f2e85e070b
deletefromuser <abc@example.com>
2018-12-26 15:48:00 +0900

    add mappers xml in mybatis

--- spring4demo/src/main/java/mybatis/MainApp.java
+++ spring4demo/src/main/java/mybatis/MainApp.java
@@ -2,6 +2,7 @@ package mybatis;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -9,6 +10,8 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import dao.Student;
+import mybatis.entity.Pet;
+import mybatis.mapper.PetMapper;
 
 public class MainApp {
 	public static void main(String[] args) {
@@ -29,6 +32,18 @@ public class MainApp {
 				System.out.print("ID : " + stu.getId());
 				System.out.print(", Name : " + stu.getName());
 				System.out.println(", Age : " + stu.getAge());
+				
+				PetMapper pm = session.getMapper(PetMapper.class);
+				Pet pet = new Pet();
+				pet.setBirth(new Date());
+				pet.setDeath(new Date());
+				pet.setName("dd");
+				pm.insert(pet);
+				
+				Pet rp = pm.selectByPrimaryKey("dd");
+				System.out.print("pet name : " + rp.getName());
+				System.out.print(", pet Birth : " + rp.getBirth());
+				System.out.println(", pet Death : " + rp.getDeath());
 			} finally {
 				session.close();
 			}
